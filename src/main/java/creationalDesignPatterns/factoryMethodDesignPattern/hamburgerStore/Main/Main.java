5ae86232b86319fd0b8cff1dafe2aafbee30256c
Rawad Alaryan <rawadalaryan@hotmail.com>
2020-06-14 16:32:46 -0400

    Minor Changes in Layout.

--- src/main/java/creationalDesignPatterns/factoryMethodDesignPattern/hamburgerStore/Main/Main.java
+++ src/main/java/creationalDesignPatterns/factoryMethodDesignPattern/hamburgerStore/Main/Main.java
@@ -11,25 +11,25 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Factory Method Design Pattern Hamburger Store.");
 		System.out.println("Hamburger Store defines a Factory Method for creating different Hamburgers (Cheese, Veggie, TorontoStyle, MontrealStyle) in its Franchise-Stores (TorontoStore, MontrealStore):\n");
-        HamburgerStore torontoBurgerStore = new TorontoHamburgerStore();
-        System.out.println("The Toronto Hamburger Store is Open for Orders!");
-        HamburgerStore montrealBurgerStore = new MontrealHamburgerStore();
-        System.out.println("The Montreal Hamburger Store is Open for Orders!\n");
-        		
-        //Let's take some orders from our clients in Toronto:
-        System.out.println("Placing an Order for a Toronto-Style Burger:");
-        Hamburger torontoStyleHamburger = torontoBurgerStore.orderHamburger("Toronto Style Burger");
-        System.out.println("\tOrder for " + torontoStyleHamburger.getType() + " is ready!\n" );
-        System.out.println("Placing an Order for a Veggie Burger:");
-        Hamburger veggieHamburger = torontoBurgerStore.orderHamburger("Veggie Burger");
-        System.out.println("\tOrder for " + veggieHamburger.getType() + " is ready!\n" );
-        
-        //Let's take some orders from our clients in Montreal:
-        System.out.println("Placing an Order for a Montreal-Style Burger:");
-        Hamburger montrealStyleHamburger = montrealBurgerStore.orderHamburger("Montreal Style Burger");
-        System.out.println("\tOrder for " + montrealStyleHamburger.getType() + " is ready!\n" );
-        System.out.println("Placing an Order for a Cheese Burger:");
-        Hamburger cheeseHamburger = montrealBurgerStore.orderHamburger("Cheese Burger");
-        System.out.println("\tOrder for " + cheeseHamburger.getType() + " is ready!\n" );
-    }
+		HamburgerStore torontoBurgerStore = new TorontoHamburgerStore();
+		System.out.println("The Toronto Hamburger Store is Open for Orders!");
+		HamburgerStore montrealBurgerStore = new MontrealHamburgerStore();
+		System.out.println("The Montreal Hamburger Store is Open for Orders!\n");
+
+		// Let's take some orders from our clients in Toronto:
+		System.out.println("Placing an Order for a Toronto-Style Burger:");
+		Hamburger torontoStyleHamburger = torontoBurgerStore.orderHamburger("Toronto Style Burger");
+		System.out.println("\tOrder for " + torontoStyleHamburger.getType() + " is ready!\n");
+		System.out.println("Placing an Order for a Veggie Burger:");
+		Hamburger veggieHamburger = torontoBurgerStore.orderHamburger("Veggie Burger");
+		System.out.println("\tOrder for " + veggieHamburger.getType() + " is ready!\n");
+
+		// Let's take some orders from our clients in Montreal:
+		System.out.println("Placing an Order for a Montreal-Style Burger:");
+		Hamburger montrealStyleHamburger = montrealBurgerStore.orderHamburger("Montreal Style Burger");
+		System.out.println("\tOrder for " + montrealStyleHamburger.getType() + " is ready!\n");
+		System.out.println("Placing an Order for a Cheese Burger:");
+		Hamburger cheeseHamburger = montrealBurgerStore.orderHamburger("Cheese Burger");
+		System.out.println("\tOrder for " + cheeseHamburger.getType() + " is ready!\n");
+	}
 }
--- src/main/java/creationalDesignPatterns/factoryMethodDesignPattern/vehicleStore/Main/Main.java
+++ src/main/java/creationalDesignPatterns/factoryMethodDesignPattern/vehicleStore/Main/Main.java
@@ -17,24 +17,24 @@ public class Main {
 		System.out.println("Factory Method Design Pattern Vehicle Store.");
 		System.out.println("Vehicle Store defines a Factory Method for creating different vehicles (Bike, Car, Truck, Crane) in its Specialized-Stores (LightVeihcleStore, HeavyVehicleStore):\n");
 		VehicleStore lightVehicleStore = new LightVehicleStore();
-        System.out.println("The Light Vehicle Store is Ready for Orders!");
-        VehicleStore heavyVehicleStore = new HeavyVehicleStore();
-        System.out.println("The Heavy Vehicle Store is Ready for Orders!\n");
-        		
-        //Let's take some orders from our light vehicles clients:
-        System.out.println("Placing an Order for a Bike:");
-        Vehicle bike = lightVehicleStore.orderVehicle(VehicleType.BIKE);
-        System.out.println("\tOrder for " + ((Bike)bike).getType() + " is ready!\n" );
-        System.out.println("Placing an Order for a Car:");
-        Vehicle car = lightVehicleStore.orderVehicle(VehicleType.CAR);
-        System.out.println("\tOrder for " + ((Car)car).getType() + " is ready!\n" );
-        
-        //Let's take some orders from our heavy vehicles clients:
-        System.out.println("Placing an Order for a Truck:");
-        Vehicle truck = heavyVehicleStore.orderVehicle(VehicleType.TRUCK);
-        System.out.println("\tOrder for " + ((Truck)truck).getType() + " is ready!\n" );
-        System.out.println("Placing an Order for a Crane:");
-        Vehicle crane = heavyVehicleStore.orderVehicle(VehicleType.CRANE);
-        System.out.println("\tOrder for " + ((Crane)crane).getType() + " is ready!\n" );
-    }	
+		System.out.println("The Light Vehicle Store is Ready for Orders!");
+		VehicleStore heavyVehicleStore = new HeavyVehicleStore();
+		System.out.println("The Heavy Vehicle Store is Ready for Orders!\n");
+
+		// Let's take some orders from our light vehicles clients:
+		System.out.println("Placing an Order for a Bike:");
+		Vehicle bike = lightVehicleStore.orderVehicle(VehicleType.BIKE);
+		System.out.println("\tOrder for " + ((Bike) bike).getType() + " is ready!\n");
+		System.out.println("Placing an Order for a Car:");
+		Vehicle car = lightVehicleStore.orderVehicle(VehicleType.CAR);
+		System.out.println("\tOrder for " + ((Car) car).getType() + " is ready!\n");
+
+		// Let's take some orders from our heavy vehicles clients:
+		System.out.println("Placing an Order for a Truck:");
+		Vehicle truck = heavyVehicleStore.orderVehicle(VehicleType.TRUCK);
+		System.out.println("\tOrder for " + ((Truck) truck).getType() + " is ready!\n");
+		System.out.println("Placing an Order for a Crane:");
+		Vehicle crane = heavyVehicleStore.orderVehicle(VehicleType.CRANE);
+		System.out.println("\tOrder for " + ((Crane) crane).getType() + " is ready!\n");
+	}
 }
