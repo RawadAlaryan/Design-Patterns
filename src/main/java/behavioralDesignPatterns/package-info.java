b5c7043dc9300d98664134ac66977b64df4322eb
Rawad Alaryan <rawadalaryan@hotmail.com>
2020-06-16 11:08:29 -0400

    Added Package-info.

---
+++ src/main/java/behavioralDesignPatterns/package-info.java
@@ -0,0 +1,22 @@
+/**	
+Behavioral Design Patterns In Java.
+<p> This package contains: </p> 
+	Chain of Responsibility Design Pattern.
+	Command Design Pattern.
+	Interpreter Design Pattern.
+	Iterator Design Pattern.
+	Mediator Design Pattern.
+	Memento Design Pattern.
+	Observer Design Pattern.
+	State Design Pattern.
+	Strategy Design Pattern.
+	Template Method Design Pattern.
+	Visitor Design Pattern.
+	Null Object Design Pattern.
+	
+@since 1.0
+@author Rawad Alaryan
+@version 1.0
+*/
+
+package behavioralDesignPatterns;
---
+++ src/main/java/structuralDesignPatterns/package-info.java
@@ -0,0 +1,18 @@
+/**	
+Structural Design Patterns In Java.
+<p> This package contains: </p> 
+	Adapter Design Pattern.
+	Bridge Design Pattern.
+	Composite Design Pattern.
+	Decorator Design Pattern.
+	Facade Design Pattern.
+	Flyweight Design Pattern.
+	Private Class Data Design Pattern.
+	Proxy Design Pattern.
+	
+@since 1.0
+@author Rawad Alaryan
+@version 1.0
+*/
+
+package structuralDesignPatterns;
